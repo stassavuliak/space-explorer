ebb9c6f811fd2de5b296b3a151c841db8eb0cdd8
Stanislav Savuliak <stassavulak@gmail.com>
2025-11-21 21:43:57 +0100

    feat: add a function getApod to load APOD

--- src/api/apod.js
+++ src/api/apod.js
@@ -1,1 +1,11 @@
 // This is just a “cell” where we will later put the function to load APOD
+
+export async function getApod() {
+  const apiKey = import.meta.env.VITE_NASA_API_KEY
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+
+  const response = await fetch(url)
+  const data = await response.json()
+
+  return data
+}

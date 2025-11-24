4271b5dac18110e7f3bf3af7a9432a49677386d8
Stanislav Savuliak <stassavulak@gmail.com>
2025-11-24 20:27:33 +0100

    refactor: add active class to Nav menu links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,24 @@
 import "./Header.scss";
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
+  const setActive = ({ isActive }) => (isActive ? "nav-link active" : "nav-link")
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrap">
-          <Link to="/" className="header__logo">
+          <NavLink to="/" className="header__logo">
             <b>S</b>pace 
             <br /> 
             <span>E</span>xplorer
-          </Link>
+          </NavLink>
 
           <nav className="header__nav">
             <ul>
-              <li><Link to="/" className="header__link">Home</Link></li>
-              <li><Link to="/rover" className="header__link">Rover</Link></li>
-              <li><Link to="/asteroids" className="header__link">Asteroids</Link></li>
+              <li><NavLink to="/" className={setActive}>Home</NavLink></li>
+              <li><NavLink to="/rover" className={setActive}>Rover</NavLink></li>
+              <li><NavLink to="/asteroids" className={setActive}>Asteroids</NavLink></li>
             </ul>
           </nav>
         </div>
